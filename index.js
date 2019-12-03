518914ca46481de9f4a0bf213cdf1a02275f0437
APaulaMiranda <apaulafmiranda@gmail.com>
2019-12-03 21:36:23 +0100

    started the connection to the database

--- index.js
+++ index.js
@@ -25,8 +25,50 @@ const postQuestion = [
 
 ];
 
+const bidQuestion = [
+
+    {
+        type: "input",
+        name: "name",
+        message: "What is the item you want to bid on?",
+    },
+    {
+        type: "input",
+        name: "price",
+        message: "How much do you want to offer?"
+    }
+
+];
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3309,
+    user: "root",
+    password: process.env.DBPASS,
+    database: process.env.DBNAME
+});
+
+// initiatte db conexion
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id ” + connection.threadId + “\n");
+    createProduct();
+});
+
+
+
 inquirer.prompt(initialQuestion).then(function (answers) {
     if (answers.postOrBid == "POST AN ITEM") {
+
+        inquirer.prompt(postQuestion).then(function (answers) {
+
+            console.log(answers)
+        })
+    }
+    else if (answers.postOrBid == "BID ON AN ITEM") {
+
+        // send query to get items , selec all from itwma
+
 
         inquirer.prompt(postQuestion).then(function (answers) {
 
